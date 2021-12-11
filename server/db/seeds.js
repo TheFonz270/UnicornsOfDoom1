3427a3909864c493b7d9b44d71c3bc12ef0c1e15
Martin Quinn <thefonz270@hotmail.com>
2021-12-11 14:28:25 +0000

    database successfully seeded

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -6,7 +6,7 @@ db.stories.insertMany([
     region: "scotland",
     character: "william_wallace",
     pages: [
-        {
+       {
         pageHeading: "Who Was William Wallace?",
         pageText:[
             "Not much is known about William Wallace's origins. He was born into a family of lesser nobility around 1270 in Elderslie, Renfrewshire. He has, however, gone on to become a major figure in Scottish history for his part in the First War of Independence. Here, you will learn about his life and the actions that have led to him having such a status.",
@@ -20,7 +20,8 @@ db.stories.insertMany([
                 "With Scotland threatening to descend into civil war, King Edward I of England tried to take power over Scotland, demanding support in his war against France.",
                 "Scottish people didn't like that.",
                 "William was one of a number of rebels who chose to uprise against Edward's rule. They began to defy their overlords with several rebellions taking place across Scotland.",
-                "That led to the beginning of the war, and the battle of Stirling Bridge."], 
+                "That led to the beginning of the war, and the battle of Stirling Bridge."
+            ], 
         pageImageUrl:"",
         pageImageAlt: "Statue of Wallace at Edinburgh Castle, By Kjetil Bjørnsrud - Own work, CC BY 2.5."
     },  {
@@ -29,7 +30,8 @@ db.stories.insertMany([
             "On 11 September 1297, although vastly outnumbered, the Scottish army led by Wallace and Andrew Moray won the Battle of Stirling Bridge.",
             "Wallace's tactics played a key role in this victory. The English army of 3,000 cavalry and roughly 10,000 infantry were outsmarted as Wallace forced them to cross Stirling Bridge. The narrow bridge prevented many soldiers from crossing together (possibly as few as three men at a time), so, while the English soldiers crossed, the Scots killed them as quickly as they could.",
             "During a major charge by the Scottish, some of the English soldiers retreated as others pushed forward, and under the overwhelming weight, the bridge collapsed and many English soldiers drowned.",
-            "After the battle, Moray and Wallace assumed the title of Guardians of the Kingdom of Scotland. In a ceremony, at the 'Kirk o' the Forest' (Selkirk), towards the end of the year, Wallace was knighted."],],
+            "After the battle, Moray and Wallace assumed the title of Guardians of the Kingdom of Scotland. In a ceremony, at the 'Kirk o' the Forest' (Selkirk), towards the end of the year, Wallace was knighted."
+        ],
         pageImageUrl:"",
         pageImageAlt:"A Victorian depiction of the battle. By Unknown author - C Hanley, History Of Scotland, Public Domain",
     },  {
@@ -47,13 +49,14 @@ db.stories.insertMany([
         pageText:[
             "Following his death, others took Wallace's mantel and continued his fight. Only 23 years after his death, in 1328, Scottish Independence was finally secured with the Treaty of Edinburgh.",
             "Wallace is still remembered to this day for standing up for what he believed in. Many monuments, plaques, movies and books have been made to keep the memory of Wallace and his actions alive.", 
-            "The Wallace Sword, which supposedly belonged to Wallace, was held for many years in Dumbarton Castle and is now in the Wallace Monument."],
+            "The Wallace Sword, which supposedly belonged to Wallace, was held for many years in Dumbarton Castle and is now in the Wallace Monument."
+        ],
         pageImageUrl:"",
         pageImageAlt:"The National Wallace Monument near Stirling. By Finlay McWalter - Own work, CC BY-SA 3.0"
     }
-]);
+]}]);
 
-db.questions.insertMany(
+db.questions.insertMany([
     {   category: "william_wallace",
         question: "When was William Wallace born?",
         correct_answer: "around 1270",
@@ -89,4 +92,4 @@ db.questions.insertMany(
             "The Tower of London",
             "Disneyland"]
     },
-)
+]);
