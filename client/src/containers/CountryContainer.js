3a5bacb1b5cbf7037cea507d48337068b5de7a6d
Martin Quinn <thefonz270@hotmail.com>
2021-12-14 16:11:09 +0000

    Timmy and bubble fully implemented

--- client/src/containers/CountryContainer.js
+++ client/src/containers/CountryContainer.js
@@ -11,6 +11,9 @@ const CountryContainer = () => {
     const [showQuiz,setShowQuiz]=useState(false)
     const [showStory,setShowStory]=useState(true)
     const [currentPage, setCurrentPage] = useState(0);
+    const [bubble, setBubble]=useState("BubbleText1.png")
+    const [showScore, setShowScore] = useState(false);
+	const [score, setScore] = useState(0);
 
     const handleNextPage = () => {
         setCurrentPage(currentPage + 1)
@@ -23,8 +26,34 @@ const CountryContainer = () => {
     const handleQuizClick = () => {
         setShowQuiz(true)
         setShowStory(false)
+        setBubble("BubbleText3.png")
     }
     // setStoryState({});
+
+    const handleTimmysBubble = () => {
+        switch(score) {
+            case 0:
+                setBubble("0of5.png")
+                break;
+            case 1:
+                setBubble("1of5.png")
+                break;
+            case 2:
+                setBubble("2of5.png")
+                break;
+            case 3:
+                setBubble("3of5.png")
+                break;
+            case 4:
+                setBubble("4of5.png")
+                break;
+            case 5:
+                setBubble("5of5.png")
+                break;
+            // default:
+          }
+        //   console.log("Bubble: ", bubble)
+    }
 
     useEffect(()=>{
     getStories().then((allStories)=>{
@@ -37,6 +66,7 @@ const CountryContainer = () => {
             setShowStory(true)
             setShowKeyPlayers(false)
             setCurrentPage(0)
+            setBubble("BubbleText2.png")
         })
     };
 
@@ -44,22 +74,52 @@ const CountryContainer = () => {
         setShowQuiz(false)
         setShowStory(false)
         setShowKeyPlayers(true)
+        setBubble("BubbleText1.png")
+        setScore(0);
+        setShowScore(false)
     }
-
+    
     const handleStory = () => {
         setShowQuiz(false)
         setShowStory(true)
         setCurrentPage(0)
+        setBubble("BubbleText2.png")
+        setScore(0);
+        setShowScore(false)
     }
 
     // console.log("In country container:", StoryState)
 
     return (
+        <>
+    <div id="HeadingBar"><div id="heading"><h1 onClick={handleHome}>Scottish History</h1></div></div>
     <div id="countryContainer">
-        {/* MAP */}
-        {showKeyPlayers?<KeyPlayers Stories={Stories} selectPlayer={selectPlayer}/>:null}
-        <CharacterComponent StoryState={StoryState} handleHome={handleHome} handleQuizClick={handleQuizClick} handleStory={handleStory} showStory={showStory} showQuiz={showQuiz} currentPage={currentPage} handleNextPage={handleNextPage} handlePrevPage={handlePrevPage}/>
+        <div class="main">
+            {showKeyPlayers?<div class="Gallery">
+                <div class="map"><img src="./imgs/Scotland.png" alt="ScotlandImage"></img></div>
+                <KeyPlayers Stories={Stories} selectPlayer={selectPlayer}/>
+            </div>:null}
+            <CharacterComponent
+                StoryState={StoryState}
+                handleHome={handleHome}
+                handleQuizClick={handleQuizClick}
+                handleStory={handleStory}
+                showStory={showStory}
+                showQuiz={showQuiz}
+                currentPage={currentPage}
+                handleNextPage={handleNextPage}
+                handlePrevPage={handlePrevPage}
+                score={score} showScore={showScore}
+                setScore={setScore}
+                setShowScore={setShowScore}
+                handleTimmysBubble={handleTimmysBubble}/>
+        </div>
+        <div class="kid">
+            <img src={`./imgs/${bubble}`}></img>
+            <img src="./imgs/timmy_transparent.png" alt="timmy"></img>
+        </div>
     </div>
+    </>
     )
 };
 
