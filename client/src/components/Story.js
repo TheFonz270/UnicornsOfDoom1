e52bbf4f2d51bf65e36ac869980b5498f488ee7b
Andrew Calder <andrewcalder8@googlemail.com>
2021-12-15 09:28:52 +0000

    css changes made

--- client/src/components/Story.js
+++ client/src/components/Story.js
@@ -23,7 +23,7 @@ const Story = ({StoryState, handleHome, currentPage, handleNextPage, handlePrevP
         <div id="buttonBar">
             {currentPage!==(0)?<button onClick={handlePrevPage} >←</button>:null}
             {currentPage!==(StoryState.pages.length-1)?<button onClick={handleNextPage} >→</button>:null}
-            {currentPage===(StoryState.pages.length-1)?<button onClick={handleQuizClick} handleHome={handleHome}>Quiz</button>:null }
+            {currentPage===(StoryState.pages.length-1)?<button onClick={handleQuizClick} handleHome={handleHome} id="quizButton">Take the Quiz</button>:null }
             <button onClick={handleHome} >Return to Gallery</button>
         </div>
     </>
